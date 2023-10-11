dc8b9045377346879ed80106922ac6f50fc2a659
Smiler-Snup <81515119+Smiler-Snup@users.noreply.github.com>
2023-10-11 17:48:49 +0300

    Added comments list header

--- FrequencyDictionary/list.h
+++ FrequencyDictionary/list.h
@@ -15,7 +15,11 @@ struct List
 
 struct Node* createNode(char* key, int value);
 
+//Инициализирует список
 void init(struct List** list);
+//Добавляет элемент в начало списка
 void push(struct List* list, char* key, int value);
+//Удаляет элемент из списка
 void removeNode(struct List* list, char* key);
+//Печатает содержимое списка на консоль
 void printList(struct List* list);
